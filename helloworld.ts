6b5122e92182f636fac910bc6f8aead824a17f74
test <110277092+shootytest@users.noreply.github.com>
2023-01-21 10:25:13 +0000

    hi world!

--- helloworld.ts
+++ helloworld.ts
@@ -1,3 +1,4 @@
 import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import { test } from "./hi.ts";
 
-serve((req: Request) => new Response("Hello World"));
+serve((req: Request) => new Response(test));
